71bfaca4101ef42acf04eeb562caecddd150f13d
Gourav Barnwal <78587654+gourav2304@users.noreply.github.com>
2024-10-20 13:29:52 +0530

    react

--- 01basicreact/src/App.js
+++ 01basicreact/src/App.js
@@ -8,11 +8,15 @@ import FunctionClick from './components/FunctionClick'
 import ClassClick from './components/ClassClick';
 import EventBind from './components/EventBind';
 import ParentComponent from './components/ParentComponent';
+import UserGreeting from './components/UserGreeting';
+import NameList from './components/NameList';
 class App extends Component {
   render(){
     return (
       <div>
-        <ParentComponent/>
+        <NameList/>
+        {/* <UserGreeting/> */}
+        {/* <ParentComponent/> */}
         {/* <EventBind /> */}
         {/* <ClassClick /> */}
         {/* <FunctionClick /> */}
